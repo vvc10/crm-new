f245a29ea2a20992262f111efc55f2348fa0d2be
vvc10 <vvastcore10@gmail.com>
2024-12-07 02:18:20 +0530

    init

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,15 +4,7 @@ import ClientDashboard from "@/pages/user/userdashboard/dashboard";
 import ClientNavbar from "@/components/navbar/ClientNavbar";
 
 const Dashboard = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);  
-  }, []);
-
-  if (!isClient) {
-    return null;  
-  }
+ 
 
   return (
     <div>
