a96c72e8d9bdad98458d77bd59a2a2a380b74cd5
vvc10 <vvastcore10@gmail.com>
2024-12-07 13:23:35 +0530

    query page added

--- src/pages/query.jsx
+++ src/pages/query.jsx
@@ -1,14 +1,14 @@
-import ClientNavbar from '@/components/navbar/ClientNavbar'
 import React from 'react'
+import ClientNavbar from '@/components/navbar/ClientNavbar'
 import ClientQuery from './user/userquery/query'
 
 const query = () => {
-  return (
-    <div>
-      <ClientNavbar/>
-      <ClientQuery/>
-    </div>
-  )
+    return (
+        <div className='query'>
+            <ClientNavbar />
+            <ClientQuery />
+        </div>
+    )
 }
 
 export default query
